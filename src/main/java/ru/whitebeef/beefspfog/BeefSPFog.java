b31b10f99f8d5a5ff044c18f9fc6bd35de95ecfc
whitebeef <whitebeef.official@gmail.com>
2021-08-20 21:39:28 +0300

    First stable version

--- src/main/java/ru/whitebeef/beefspfog/BeefSPFog.java
+++ src/main/java/ru/whitebeef/beefspfog/BeefSPFog.java
@@ -1,40 +1,99 @@
 package ru.whitebeef.beefspfog;
 
+import io.papermc.paper.text.PaperComponents;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.world.TimeSkipEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 public final class BeefSPFog extends JavaPlugin implements Listener {
 
-    private HashMap<World, Fog> fogs = new HashMap<World, Fog>();
+    private HashMap<World, Fog> fogs = new HashMap<>();
+    private static List<String> deathMessages;
+
+
+    // Инициализация сообщений о смерти
+    static {
+        String[] messagesArray = {
+                "задохнулся в тумане",
+                "был поглощён туманом",
+                "заплутал в тумане",
+                "растворился в тумане"
+        };
+        deathMessages = Arrays.asList(messagesArray);
+    }
 
     @Override
     public void onEnable() {
+
         Bukkit.getPluginManager().registerEvents(this, this);
-        Bukkit.getWorlds().forEach(world -> fogs.put(world, new Fog(world, 100, 4)));
-        // Показ партиклов дл игрока и обновление высоты тумана
+
+        // Гернерация фога для миров
+        Fog overworldFog = new Fog(Bukkit.getWorlds().get(0), 100, 3);
+        Fog netherFog = new Fog(Bukkit.getWorlds().get(1), 64, 3);
+        Fog endFog = new Fog(Bukkit.getWorlds().get(2), 80, 3);
+
+        fogs.put(overworldFog.getWorld(), overworldFog);
+        fogs.put(netherFog.getWorld(), netherFog);
+        fogs.put(endFog.getWorld(), endFog);
+
+
+        // Показ партиклов для игрока и обновление высоты тумана
         Bukkit.getScheduler().runTaskTimerAsynchronously(this, () -> {
             Bukkit.getWorlds().forEach(world -> fogs.get(world).updateHeight());
             Bukkit.getOnlinePlayers().forEach(player -> fogs.get(player.getWorld()).showParticles(player));
         }, 0, 5l);
 
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
+        // Нанесение дамага игроку и показ ему эффекта слепоты
+        Bukkit.getScheduler().runTaskTimer(this, () -> {
             Bukkit.getOnlinePlayers().forEach(player -> fogs.get(player.getWorld()).damage(player));
-
         }, 0l, 20l);
+
+        // Показ
+        Bukkit.getScheduler().runTaskTimer(this, () -> {
+            Bukkit.getOnlinePlayers().forEach(player -> {
+                if (player.getInventory().getItemInOffHand().getType() == Material.CLOCK ||
+                        player.getInventory().getItemInMainHand().getType() == Material.CLOCK) {
+                    int avgFogHeight = (int) (fogs.get(player.getWorld()).getHeight() / 10) * 10;
+                    player.sendActionBar(PaperComponents.legacySectionSerializer()
+                            .deserialize(ChatColor.translateAlternateColorCodes('&',
+                                    "&7Туман : " + avgFogHeight + " - " + (avgFogHeight + 10))));
+                }
+            });
+
+        }, 0l, 10l);
+
     }
 
-
+    // Предотвращение скипа ночи
     @EventHandler
     public void onNightSkip(TimeSkipEvent event) {
         if (event.getSkipReason() == TimeSkipEvent.SkipReason.NIGHT_SKIP)
             event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        if (event.getEntity().getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.CUSTOM)
+            event.deathMessage(getDeathMessage(event.getEntity()));
+    }
+
+    private Component getDeathMessage(Player player) {
+        return PaperComponents.legacySectionSerializer()
+                .deserialize(player.getName() + " " + deathMessages.get(new Random().nextInt(deathMessages.size())));
     }
 
     @Override
@@ -42,7 +101,3 @@ public final class BeefSPFog extends JavaPlugin implements Listener {
 
     }
 }
-
-// CLOUD
-// c 6500 до 17000 до 100
-// с 19000 до 5500 до 0
--- src/main/java/ru/whitebeef/beefspfog/Fog.java
+++ src/main/java/ru/whitebeef/beefspfog/Fog.java
@@ -104,7 +104,7 @@ public class Fog {
      * @param player Игрок, урон которому необходимо нанести
      */
     public Fog damage(Player player) {
-        if (player.getLocation().getY() > height) return this;
+        if (player.getEyeLocation().getY() > height) return this;
         if (player.getGameMode().equals(GameMode.SPECTATOR))
             return this;
         applyEffects(player);
@@ -132,43 +132,56 @@ public class Fog {
         long t = Bukkit.getWorlds().get(0).getTime();
         double height;
 
-        // [5500-6500] - туман чисто IDLE-шит на 0ой высоте
-        // [6500-17500] - период поднятия тумана
-        // [17500-18500] - туман чисто IDLE-шит на 100ой высоте
-        // [18500 - 5500] - период спуска тумана
+        // [5500 - 6500] - туман чисто IDLE-шит на 0ой высоте
+        // [6500 - 16500] - период поднятия тумана
+        // [16500 - 19500] - туман чисто IDLE-шит на 100ой высоте
+        // [19500 - 5500] - период спуска тумана
 
-        if (t >= 6500 && t <= 17500)
-            height = (t - 6500d) / (17500d - 6500d) * 100;
-        else if (t >= 18500 || t <= 5500) {
+        if (t >= 6500 && t <= 16500)
+            height = (t - 6500d) / (16500d - 6500d) * maxHeight;
+        else if (t >= 19500 || t <= 5500) {
             if (t <= 5500)
                 t += 24000;
-            height = (29500d - t) / (29500d - 18500d) * 100;
+            height = (29500d - t) / (29500d - 19500d) * maxHeight;
         } else if (t < 6500)
             height = 0;
         else
-            height = 100;
+            height = maxHeight;
         this.height = height;
     }
 
-
+    /**
+     * Показ партиклов тумана игроку
+     *
+     * @param player Игрок, которому отправляется партикл
+     */
     public void showParticles(Player player) {
-        Location playerLocation = player.getLocation().clone();
-        double radius = playerLocation.getY() < height ? 15 : 60 + (player.getEyeLocation().getY() - this.height) / 2d;
+        Location playerLocation = player.getEyeLocation().clone();
+        double radius = playerLocation.getY() < height ? 15 : 40 + (player.getEyeLocation().getY() - this.height);
         if (playerLocation.getY() < height) {
             player.spawnParticle(Particle.CLOUD, player.getEyeLocation(), 25, 5, 5, 5, 0.1);
         } else
             for (int y = (int) height; y > height - 20 && y > 0; y -= 3) {
-                for (int i = 0; i < (y == height ? 50 : 8); i++) {
-                    Location location = getRandomLocation(playerLocation, radius);
-                    location.setY(y);
-                    if (!location.getBlock().getType().isSolid())
-                        spawnParticle(player, location);
+                for (int i = 0; i < (y == height ? 50 : 10); i++) {
+                    for (int k = 0; k < 10; k++) {
+                        Location location = getRandomLocation(playerLocation, radius);
+                        location.setY(y);
+                        if (!location.getBlock().getType().isSolid()) {
+                            spawnParticle(player, location);
+                            break;
+                        }
+                    }
                 }
             }
     }
 
+    /**
+     * Получение рандомной локации в рамках круга
+     *
+     * @param center центр круга
+     * @param radius радиус круга
+     */
     private Location getRandomLocation(Location center, double radius) {
-
         double a = rnd.nextDouble() * 2 * Math.PI;
         double r = rnd.nextDouble() * radius;
         double x = center.getX() + r * Math.cos(a);
@@ -176,7 +189,13 @@ public class Fog {
         return new Location(center.getWorld(), x, center.getY(), z);
     }
 
+    /**
+     * Спавн партикла
+     *
+     * @param player   игрок, которому показывается частица
+     * @param location локация, на которой будет показываться частица
+     */
     private void spawnParticle(Player player, Location location) {
-        player.spawnParticle(Particle.CLOUD, location, 1, 0, 1, 0, 0.05);
+        player.spawnParticle(Particle.CLOUD, location, 1, 0, 0, 0, 0.05);
     }
 }
